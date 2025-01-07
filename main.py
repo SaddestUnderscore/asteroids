1a7a4c7b4088ccdc788c74aecba0f799ba82ed13
SaddestUnderscore <156387855+SaddestUnderscore@users.noreply.github.com>
2025-01-07 04:40:05 +0300

    that was painful to set up, but we got a screen

---
+++ main.py
@@ -0,0 +1,22 @@
+from constants import *
+import pygame
+
+print("Starting asteroids!")
+print(pygame.__version__)
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
