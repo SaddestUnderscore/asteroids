2bcc4ccf017e7b71c91523555969fe3350ec5f0d
SaddestUnderscore <156387855+SaddestUnderscore@users.noreply.github.com>
2025-01-07 06:02:39 +0300

    rotatable player

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -20,9 +20,10 @@ def main():
                 return
             
         screen.fill((0, 0, 0))
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        player.draw(screen)
         dt = clock.tick(60) / 1000
+
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
 
 
