15db3506031a54ee882027193a82e3c53cff1add
SaddestUnderscore <156387855+SaddestUnderscore@users.noreply.github.com>
2025-01-07 05:08:37 +0300

    add delta time, add circleshape

---
+++ circleshape.py
@@ -0,0 +1,21 @@
+import pygame
+
+# the base class object for other game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+        # sub classes must override
+        def draw(self, screen):
+            pass
+
+        def update(self, dt):
+            pass
--- main.py
+++ main.py
@@ -8,6 +8,10 @@ print(f"Screen height: {SCREEN_HEIGHT}")
 
 def main():
     pygame.init()
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -15,6 +19,7 @@ def main():
                 return
             
         screen.fill((0, 0, 0))
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 
